9866507af1b41198df4e6443b58916690281b92a
Brett Jensen <bretjensen@gmail.com>
2020-12-10 08:40:03 +0000

    Allow us to show al snapshots, not just the most recent

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -31,7 +31,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
               help='Only volumes for project (tag Project:<name>)')
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+              help='List alll snapshots for each volume, not just the most recent.')
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -47,6 +49,9 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all:
+                    break
     return
 
 
